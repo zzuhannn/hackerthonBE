2a588554c1be8174b17185dd279a7af0c56fd53c
zzuhannn <lunaaeric41@naver.com>
2025-07-17 14:33:15 +0900

    :gear:Setting

---
+++ src/test/java/com/example/hackerthonbe/HackerthonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackerthonbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackerthonBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
